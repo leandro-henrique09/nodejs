c087f77e97982177fb0006e42990fad0b430b875
leandro-henrique09 <leandrohdc09@gmail.com>
2026-07-27 13:48:39 -0300

    estudando continue e break

---
+++ logica_de_programação/continue_e_break/continue.js
@@ -0,0 +1,30 @@
+let pares = [];
+let impares = [];
+const numeros = [1,2,3,4,5,6,7,8,9];
+
+for (let numero of numeros) {
+    if (numero % 2 == 0){
+        pares.push(numero);
+        // o continue é usado para mandar o laço 
+        // "pular para o próximo" item a ser percorrido
+        // neste caso quando chegar em um numero que é par ele vai pular para o próximo.
+        continue;
+    }
+    impares.push(numero);
+}
+
+// console.log(`Pares: ${pares}\nImpares: ${impares}`);
+
+// =======
+
+for (let numero of numeros) {
+    if(numero == 4) {
+        console.log("o número 4 foi encontrado.");
+        // neste caso o break manda o laço parar
+        //  quando um item for encontrado no laço
+        // como no exemplo o número 4.
+        break;
+    }
+    console.log(numero);
+}
+
